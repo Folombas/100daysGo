4d086df4eb1f680f7bc36ae5f8261a695e4877d1
Folombas <folombas@yandex.ru>
2025-07-29 09:47:31 +0300

    updated README

--- day4/day4.go
+++ day4/day4.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	// Получаем текущую дату
+	now := time.Now()
+
+	// Словарь для перевода месяцев на русский (в родительном падеже)
+	months := map[time.Month]string{
+		time.January:   "января",
+		time.February:  "февраля",
+		time.March:     "марта",
+		time.April:     "апреля",
+		time.May:       "мая",
+		time.June:      "июня",
+		time.July:      "июля",
+		time.August:    "августа",
+		time.September: "сентября",
+		time.October:   "октября",
+		time.November:  "ноября",
+		time.December:  "декабря",
+	}
+
+	// Извлекаем компоненты даты
+	day := now.Day()
+	month := months[now.Month()]
+	year := now.Year()
+
+	// Форматируем вывод согласно примеру
+	fmt.Printf("Сегодня %d %s %d г.\n", day, month, year)
 	fmt.Println("This is Day 4 of 100 days of daily Go coding!")
 }
