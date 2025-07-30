8fa3be686debc3f6912259ae829654a82f6cb058
Folombas <folombas@yandex.ru>
2025-07-30 20:06:37 +0300

    Я уставший после изматывающих заказиков на ржавом велике, продолжаю заниматься програмированием на Go!

--- day5/day5.go
+++ day5/day5.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("I continue to learn Go! Today is day 5 to my it-challenge!")
+	fmt.Println("Я уставший после изматывающих заказиков на ржавом велике, продолжаю заниматься програмированием на Go!")
 }
