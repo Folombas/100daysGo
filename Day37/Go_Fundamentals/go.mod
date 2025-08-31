ae89fea25d7a8f0f9add6e0d1265d8f39f72374a
Folombas <folombas@yandex.ru>
2025-08-31 15:21:08 +0300

    Add Day37: Introduction to Go fundamentals - syntax, types, and control structures

---
+++ Day37/Go_Fundamentals/go.mod
@@ -0,0 +1,3 @@
+module Go_Fundamentals
+
+go 1.25.0
---
+++ Day37/Go_Fundamentals/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("День 37: Основы Go - Синтаксис, Типы и Управляющие Конструкции")
+	fmt.Println("===============================================================")
+
+	DemonstrateBasics()
+	DemonstrateFunctions()
+	DemonstrateControlStructures()
+
+	fmt.Println("Изучение основ завершено! 🎉")
+}
