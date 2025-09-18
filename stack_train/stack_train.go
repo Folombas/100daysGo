e78e4e58eaedbeafe63309426f27e6b2cb5a3d3b
Folombas <folombas@yandex.ru>
2025-09-18 13:54:29 +0300

    My learning stack for the coming months

---
+++ stack_train/stack_train.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// Твой стек на ближайшие месяцы:
+priorityStack := []string{
+    "Go",
+    "Git",
+    "Docker & Kubernetes",
+    "Linux & Bash",
+    "Databases (PostgreSQL, Redis)",
+    "Message brokers (Kafka/RabbitMQ)",
+}
+println(priorityStack)
+}
