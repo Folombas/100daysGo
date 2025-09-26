9a18edfb2bfa619d1dfc5973d02f6f4818ad1ccf
Folombas <folombas@yandex.ru>
2025-09-26 13:31:57 +0300

    Сегодня днём 26 сентября 2025 мы изучаем goto (discouraged) в Go

--- day_counter/day_counter.go
+++ day_counter/day_counter.go
@@ -45,5 +45,5 @@ func main() {
 			fmt.Printf("%s: Идеальный день для изучения чего-то ещё в Go!\n", dayLabel)
 		}
 	}
-	fmt.Println("Сегодня вечером 25 сентября 2025 мы изучаем Pointer Basics в Go.")
+	fmt.Println("Сегодня днём 26 сентября 2025 мы изучаем goto (discouraged) в Go.")
 }
