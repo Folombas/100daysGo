ba0db3a35e8a9cdbd49f9f04ef7a9030b514ecfd
Folombas <folombas@yandex.ru>
2025-11-05 21:38:52 +0300

    Added file main: Go Environment Setup

---
+++ day2/setting-up-environment/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("🛠️ Day 2: Go Environment Setup")
+    fmt.Println("===============================")
+
+    CheckGoEnvironment()
+    CompareWithOtherLanguages()
+    DemoGoTools()
+}
