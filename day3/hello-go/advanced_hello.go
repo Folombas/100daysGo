f2f44179de501ec1bb0de27e26b6b5fd9faa6c92
Folombas <folombas@yandex.ru>
2025-11-06 21:47:28 +0300

    🌟 DAY3: Fix date and version output | Use actual Go version and full date | Add launch time

--- day3/hello-go/advanced_hello.go
+++ day3/hello-go/advanced_hello.go
@@ -1,17 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
 
 func ShowAdvancedHello() {
 	fmt.Println("\n2. 💫 Продвинутый Hello World:")
 
 	// Разные способы вывода
 	name := "Gopher"
-	version := 1.21
+	currentTime := time.Now()
+
+	// Получаем реальную версию Go
+	goVersion := runtime.Version()
 
 	fmt.Printf("   Hello, %s! \n", name)
-	fmt.Printf("   Go version: %.2f \n", float64(version))
-	fmt.Printf("   Сегодняшняя дата: 2025 \n")
+	fmt.Printf("   Go version: %s \n", goVersion)
+	fmt.Printf("   Сегодняшняя дата: %s\n", currentTime.Format("02.01.2006"))
+	fmt.Printf("   Время запуска: %s\n", currentTime.Format("15:04:05"))
 
 	// Многострочный вывод
 	message := `
