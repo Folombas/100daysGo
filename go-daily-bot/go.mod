c6ccb653130a56227e1fc6dd9d1ba8b54ae15630
Folombas <folombas@yandex.ru>
2025-11-06 23:14:21 +0300

    Updated system_info

--- go-daily-bot/go.mod
+++ go-daily-bot/go.mod
@@ -2,4 +2,7 @@ module go-daily-bot
 
 go 1.25.3
 
-require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/joho/godotenv v1.5.1
+)
--- go-daily-bot/system_info.go
+++ go-daily-bot/system_info.go
@@ -7,33 +7,47 @@ import (
 )
 
 type SystemInfo struct {
-	OS          string
-	GoVersion   string
+	OS           string
+	GoVersion    string
 	Architecture string
-	NumCPU      int
+	NumCPU       int
+	StartTime    time.Time
 }
 
-func NewSystemInfo() *SystemInfo {
+func NewSystemInfo(startTime time.Time) *SystemInfo {
 	return &SystemInfo{
-		OS:          runtime.GOOS,
-		GoVersion:   runtime.Version(),
+		OS:           runtime.GOOS,
+		GoVersion:    runtime.Version(),
 		Architecture: runtime.GOARCH,
-		NumCPU:      runtime.NumCPU(),
+		NumCPU:       runtime.NumCPU(),
+		StartTime:    startTime,
 	}
 }
 
 func (si *SystemInfo) GetSystemMessage() string {
+	uptime := time.Since(si.StartTime)
+
 	message := "💻 *Информация о системе:*\n\n"
 	message += fmt.Sprintf("⚙️  *ОС:* %s\n", si.OS)
 	message += fmt.Sprintf("🚀 *Архитектура:* %s\n", si.Architecture)
 	message += fmt.Sprintf("🔢 *Процессоры:* %d\n", si.NumCPU)
 	message += fmt.Sprintf("🐹 *Версия Go:* %s\n", si.GoVersion)
-	message += fmt.Sprintf("⏰ *Аптайм:* %s", si.getUptime())
+	message += fmt.Sprintf("⏰ *Аптайм:* %s\n", si.formatUptime(uptime))
+	message += fmt.Sprintf("🕒 *Время запуска:* %s", si.StartTime.Format("02.01.2006 15:04:05"))
 
 	return message
 }
 
-func (si *SystemInfo) getUptime() string {
-	// Для демонстрации - случайное время
-	return "2 часа 15 минут"
+func (si *SystemInfo) formatUptime(uptime time.Duration) string {
+	hours := int(uptime.Hours())
+	minutes := int(uptime.Minutes()) % 60
+	seconds := int(uptime.Seconds()) % 60
+
+	if hours > 0 {
+		return fmt.Sprintf("%d ч %d мин %d сек", hours, minutes, seconds)
+	} else if minutes > 0 {
+		return fmt.Sprintf("%d мин %d сек", minutes, seconds)
+	} else {
+		return fmt.Sprintf("%d сек", seconds)
+	}
 }
