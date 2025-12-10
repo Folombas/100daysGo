615223afaadc4cc6779d414192c256b86280d9e0
Folombas <folombas@yandex.ru>
2025-12-10 20:46:58 +0300

    📅 10.12.2025 | День: 46/100 | Тема: Conditionals

--- day_counter/main.go
+++ day_counter/main.go
@@ -38,7 +38,7 @@ var (
 	gosha      = Person{"Гоша", 38, "Бывший рэпер-гламурщик", "Стать Go-разработчиком"}
 	currentDay = daysSince(startDateStr)
 	progress   = calculateProgress()
-	theme      = "Embedding Structs"
+	theme      = "Conditionals"
 	r          = rand.New(rand.NewPCG(uint64(time.Now().UnixNano()), uint64(time.Now().UnixNano()>>32)))
 )
 
