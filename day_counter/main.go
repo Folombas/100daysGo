b15d7fc2c771fbddf2c7a267b691a43726426d63
Folombas <folombas@yandex.ru>
2025-12-29 22:55:45 +0300

    📅 29.12.2025 | День: 56/100 | Тема: Methods and Interfaces

--- day_counter/main.go
+++ day_counter/main.go
@@ -65,7 +65,7 @@ func NewApp() *App {
 		},
 		currentDay: currentDay,
 		progress:   progress,
-		theme:      "Get a Brief Overview: Garbage Collection",
+		theme:      "Methods and Interfaces",
 		prng:       rand.New(rand.NewSource(time.Now().UnixNano())),
 		motivations: []string{
 			"20 лет назад ты носил коробки за 250₽. Сегодня — за те же 250₽. С учётом инфляции это в 4 РАЗА меньше реальной стоимости! В IT зарплата растёт с каждым проектом.",
