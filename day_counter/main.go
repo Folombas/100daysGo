a07e9b10c9a1d4ffe4837e3cd0ff0ff5ae575da6
Alex <folombas@yandex.ru>
2026-01-20 22:44:55 +0300

    Сегодня поздно вечером изучаем тему: Code organization: Error Handling: Error Handling Basics

--- day_counter/main.go
+++ day_counter/main.go
@@ -43,7 +43,7 @@ var (
 	}
 
 	todayTopic = DailyTopic{
-		Title:      "Code organization: Modules & Dependencies: go mod vendor",
+		Title:      "Code organization: Error Handling: Error Handling Basics",
 		MinLines:   100,
 		Category:   "Go Core",
 		Complexity: "Intermediate",
