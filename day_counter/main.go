a7cfb9544deb4b9f003c1cbb46b302a2d8261143
Alex <folombas@yandex.ru>
2026-01-26 22:59:59 +0300

    Сегодня поздно вечером мы изучаем тему: Code organization: Error Handling: panic and recover

--- day_counter/main.go
+++ day_counter/main.go
@@ -43,7 +43,7 @@ var (
 	}
 
 	todayTopic = DailyTopic{
-		Title:      "Code organization: Error Handling: Sentinel Errors",
+		Title:      "Code organization: Error Handling: panic and recover",
 		MinLines:   100,
 		Category:   "Go Core",
 		Complexity: "Intermediate",
