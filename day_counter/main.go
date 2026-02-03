76fc9da2697d7a2952b2bc5763045a5a8b44738e
Alex <folombas@yandex.ru>
2026-02-03 20:13:09 +0300

    Сегодня вечером мы изучаем тему: Context package in Golang

--- day_counter/main.go
+++ day_counter/main.go
@@ -43,7 +43,7 @@ var (
 	}
 
 	todayTopic = DailyTopic{
-		Title:      "Common Usecases in Golang Programming",
+		Title:      "Context package in Golang",
 		MinLines:   100,
 		Category:   "Go Core",
 		Complexity: "Intermediate",
